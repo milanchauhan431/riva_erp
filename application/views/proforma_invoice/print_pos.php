e08e90adc164ff351ad3cbadea6b108f9448b2e7
Piyush <modhavadiya55@gmail.com>
2023-10-16 12:32:23 +0530

    PI

--- application/views/proforma_invoice/print_pos.php
+++ application/views/proforma_invoice/print_pos.php
@@ -54,6 +54,9 @@
                         echo '<strong>HSN/SAC:</strong> ' . $row->hsn_code . '<br>';
                         echo '<strong>Qty:</strong> ' . floatVal($row->qty) . ' (' . $row->unit_name . ')<br>';
                         echo '<strong>Rate:</strong> ' . floatVal($row->price) . '<br>';
+                        if(!empty($row->gold_platinum_price)):
+                            echo '<strong>Gold/Platinum Amount :</strong> ' . floatVal($row->gold_platinum_price) . '<br>';
+                        endif;
                         echo '<strong>Making Charge:</strong> ' . floatVal($row->making_charge - $row->making_charge_dicount) . '<br>';
                         echo '<strong>Disc (%):</strong> ' . floatVal($row->disc_per) . '<br>';
                         echo '<strong>GST(%):</strong> ' . $row->gst_per . '<br>';
