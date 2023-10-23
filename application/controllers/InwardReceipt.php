fc58e9f329f0f093b2c5d386a8e3b2b6104c2bd1
Piyush <modhavadiya55@gmail.com>
2023-10-23 19:50:18 +0530

    Update InwardReceipt.php

--- application/controllers/InwardReceipt.php
+++ application/controllers/InwardReceipt.php
@@ -382,7 +382,8 @@ class InwardReceipt extends MY_Controller
             else :
                 $data['attachments'] = "";
             endif;
-
+			$data['gold_weight'] = $data['gross_weight'] - $data['net_weight'] - $data['kundan_weight'] 
+			- $data['stone_weight'] - $data['moti_weight']-  $data['diamond_weight'];
             $this->printJson($this->inwardReceipt->save($data));
         endif;
     }
